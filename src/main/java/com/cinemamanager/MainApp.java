b557fa6d960204d52c2679fcd1f7c7569cf9fe58
bobanvesin@gmail.com <bobanvesin@gmail.com>
2025-09-17 14:37:24 +0200

    Database access corrected
    The app connects to the correct database now

--- src/main/java/com/cinemamanager/MainApp.java
+++ src/main/java/com/cinemamanager/MainApp.java
@@ -45,8 +45,6 @@ public class MainApp extends Application {
 		// Create the main view (layout + controllers are bootstrapped inside MainView)
 		MainView mainView = new MainView();
 
-		System.out.print("jshdgfvjy ");
-
 		// Define the scene with specified width and height
 		Scene scene = new Scene(mainView, 1200, 600);
 
